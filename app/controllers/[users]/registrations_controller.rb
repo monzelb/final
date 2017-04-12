b89c019d285818c4554a14b15f5e5f829accb58a
monzelb <monzelb@gmail.com>
2017-04-11 17:21:59 -0700

    fixed naming issue in devise controllers

--- app/controllers/[users]/registrations_controller.rb
+++ app/controllers/[users]/registrations_controller.rb
@@ -1,4 +1,4 @@
-class [users]::RegistrationsController < Devise::RegistrationsController
+class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
