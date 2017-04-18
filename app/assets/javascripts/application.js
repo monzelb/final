e38c2fec461380d1d8411b79ed75cd6d10686d05
monzelb <monzelb@gmail.com>
2017-04-18 15:37:00 -0700

    tunred off turbolinks to fix scrolling sidebar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,14 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
 
-$(document).ready(function () {
-$('#nav').affix({
-    offset: {
-        top: $('#nav').offset().top
-    }
-});
-
-})
